8d2e95ae0dc1150466c958c0f1bda5c401587195
DoddsDonnelly <crystaldodds@msn.com>
2015-10-12 10:56:04 -0700

    Chapter 7 complete, working, and notated.

---
+++ Chapter 7/script/controllers/sportsStore.js
@@ -0,0 +1,25 @@
+﻿// by omiting [] you are telling the module that you want to locate a module that has already been defined. 
+// this controller defines data that will be used in different views 
+// controllers can be arranged in a hierarchy, and will inherit data and logic from the controller above them.
+// this is a top-level controller
+angular.module("sportsStore")
+    // Setting up the Ajax request
+    // Ajax requests a regular HTTP request that happens asynchronously (in the background).  AngularJS represents a asynchronous operations using promises.   
+    .constant("dataUrl", "http://localhost:2403/products")
+    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+
+        $scope.data = {};
+
+        // HTTP GET method to request the URL passes as an argument.  
+        // HTTP GET method starts the request.  It returns an object that defines both the success and error methods.
+        // AngularJS promises to call one of the methods to let you know how the request turned out. 
+        $http.get(dataUrl)
+            // Runs if there is no problem.  Also converts the JSON data to JavaScript objects. 
+            .success(function (data) {
+                $scope.data.products = data;
+            })
+            // Runs if there is a problem
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+});
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Pro-AngularJS
-Book examples from Pro AngularJS by Adam Freeman
+Book examples from Pro AngularJS by Adam Freeman.
+ 
+All examples and notes are taken directly from referenced book.  
+ 
+Project assumes that you have gone through chapter one and installed all required programs - Node.js, Web Server, Test System, AngularJS, Bootstrap, and Deployd.  
+ 
+This particular project also required Visual Studios be installed.  
+ 
+Each chapter is setup as its own website, simply right click the solution in visual studio and select properties.  Under startup project each chapter has an action property set the interested chapter to start.  Once setup that particular chapter should build and run.  
+
+Some chapters require an active database to be run as well.  In those cases Deloyed must be initiated.  From file explorer navigate to the project folder, as well as the particular chapter.  Under data double click the app.dpb icon to start the service.  
+
+
